2eb6284ae5b90161d07ccb1ea97bd826532062e7
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-05-03 22:55:38 +0530

    Create sol5.cpp

---
+++ assignment1/sol5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int a[1000];
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+    }
+    
+    int sum  = 0;
+     for(int i=0; i<n; i++){
+         sum =sum+ a[i];
+     }
