2b3162e6b6b9c9ebc1c98ec240ebf9212a6ae01a
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-05-04 22:48:35 +0530

    Create sol7.cpp

---
+++ assignment2/sol7.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<=1){
+       return 0; 
+    }
+    else
+    for (int i=2;i<n;i++){
+      if (n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+    
+}
+int main() {
+    int p;
+    cin>>p;
+    if (isPrime(p)==0){
+        cout<<"not prime";
+        
+    }
+    else {
+        cout<<"prime";
+    }
+}
