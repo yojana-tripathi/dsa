944c7e75c888b7e6633f4892e2910235b27eacfa
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-05-09 22:45:48 +0530

    Create sol3.cpp

---
+++ assignment 4/sol3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int p = 1 , sum = 0;
+        
+        while(n>0){
+            p*= n%10;
+            sum+= n%10;
+            n = n/10;
+        }
+        
+        return  p - sum;
+        
+    }
+    
+   
+    
+};
