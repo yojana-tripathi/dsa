90f9e983f328e7d1b57dd68325b0adc80db71ae6
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-05-15 19:52:51 +0530

    Create insertionsort2.cpp

---
+++ assignment 7/insertionsort2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion2(vector<int>a){
+    int i=0,j,key;
+    for(int i=1;i<a.size();i++){
+        j=i-1;
+        key=a[i];
+        while(j>=0 && key<a[j]){
+            a[j+1]=a[j];
+            j--;  
+     }
+        a[j+1]=key;
+        for(int i=0;i<a.size();i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<"\n";
+    }
+
+}
+int main(){
+    int n,e;
+    cin>>n;
+    vector<int>a;
+    for(int i=0;i<n;i++){
+        cin>>e;
+        a.push_back(e);
+    }
+    insertion2(a);
+}
+
