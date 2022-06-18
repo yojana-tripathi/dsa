2484d6f0e52f0a08d5aeb24947baa0fcacfab709
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-06-18 19:53:30 +0530

    Create sol4.cpp

---
+++ assignment 10/sol4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n, mid;
+        while(low<=high){
+            mid = low+(high-low)/2;
+            if(guess(mid) == 0){
+                return mid;
+            }
+            else if(guess(mid) ==1){
+                 low = mid+1;
+            }
+              
+            else{
+            high = mid-1;
+            }
+        }
+          return 0;
+
+    }
+    
+};
