0fc843744e8322ac48e7a329c91d2db643d28b0c
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-06-19 20:59:15 +0530

    Create sol4.cpp

---
+++ assignment 14/sol4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int i=n; i>=1; i =i/2){
+        for(int j =1; j<=i; j++){
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+}
