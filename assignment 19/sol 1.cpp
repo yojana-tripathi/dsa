382acea785d0ad8afe836c86539127b8011f33c0
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-10-18 22:24:45 +0530

    Create sol 1.cpp

---
+++ assignment 19/sol 1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+      int start = 0;
+        int end = nums.size()-1;
+        if(end==0) return nums[0];
+        else if(nums[0]!=nums[1])return nums[0];
+        else if(nums[end]!= nums[end-1]) return nums[end];
+        
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]!=nums[mid-1] ){
+                return nums[mid];
+            }
+            if((mid%2)==1 && nums[mid]==nums[mid-1]){
+                start = mid+1;
+            }
+            else{ 
+                end = mid-1;
+            }
+        }
+        return 1;
+    }
+};
