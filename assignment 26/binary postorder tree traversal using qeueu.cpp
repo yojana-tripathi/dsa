7e52dacc320fc5cf68a7d8cfba735f6ebec09eaa
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-10-22 18:43:49 +0530

    Create binary postorder tree traversal using qeueu.cpp

---
+++ assignment 26/binary postorder tree traversal using qeueu.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+     vector<int>s;
+    vector<int> postorderTraversal(TreeNode* root) {
+        queue<TreeNode*>Q;
+        TreeNode*temp;
+        if(root==NULL) return s;
+        Q.push(root);
+        while(!Q.empty()){
+            temp=Q.front();
+            Q.pop();
+            if(temp->left!=NULL) {
+                postorderTraversal(temp->left);
+            }
+            if(temp->right!=NULL){
+                postorderTraversal(temp->right);
+               
+            }
+             s.push_back(temp->val);
+        }
+        return s;
+    }
+};
+
