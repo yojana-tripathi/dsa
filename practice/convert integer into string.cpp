d75642547a49a3645bf7787d4c8352c31f6e715a
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-11-03 23:40:35 +0530

    Update convert integer into string.cpp

--- practice/convert integer into string.cpp
+++ practice/convert integer into string.cpp
@@ -8,3 +8,17 @@ int main(){
     cout<<"\n";
     cout<<"value of n is:"<<n<<"\n";
 }
+
+// # using to_string method:
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n1 = 21;
+    int n2  =2134; 
+    cin>>n1>>n2;
+    string str1 = to_string(n1);
+    string str2 = to_string(n2);
+    cout<<"value of n1 is:"<<str1<<"\n";
+    cout<<"value of n2 is:"<<str2<<"\n";
+    
+}
