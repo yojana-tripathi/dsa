096969e3f4c0484bb32df38f29e9b2f84d11262c
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2022-11-07 04:44:28 +0530

    Update isrectangle.cpp

--- assignment-2math/isrectangle.cpp
+++ assignment-2math/isrectangle.cpp
@@ -2,6 +2,9 @@ int Solution::solve(int A, int B, int C, int D) {
     if(A==B && C==D){
         return 1;
     }
+    if(A==C && B==D){
+        return 1;
+    }
      else{
          return 0;
      }
