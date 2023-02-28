984b7a931e545eb4b08ea29e7cfce85fa5f6f109
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2023-02-28 11:37:06 +0530

    Create remove duplivate from sorted list.cpp

---
+++ treehackerrankandleetcode/remove duplivate from sorted list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return head;
+        ListNode* curr = head; 
+        while(curr !=NULL && curr->next !=NULL){
+            if(curr->val ==  curr->next->val){
+            curr->next = curr->next->next;
+            }
+            else
+                curr = curr->next;
+                }
+                   return head;
+            }
+};
