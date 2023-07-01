3640d4bb42c424d144b274f43efb1554b230acf6
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2023-07-01 18:42:20 +0530

    Update removeelement.cpp

--- array/removeelement.cpp
+++ array/removeelement.cpp
@@ -8,3 +8,21 @@ public:
         return i;
     }
 };
+
+// second approach
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] == val){
+                nums[i] = nums[n-1];
+                i--;
+                n--;
+            }
+        }
+      return n;
+        
+    }
+};
